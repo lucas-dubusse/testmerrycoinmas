8d7875bf37247e9e757f531988539a9a06fa4e30
Lucas DUBUSSE <lucasd.lecandos@gmail.com>
2024-12-23 01:20:52 +0100

    Update backend.js

--- backend.js
+++ backend.js
@@ -111,7 +111,7 @@ app.get('/total-pledged', async (req, res) => {
     );
     await connection.end();
     const total = rows[0].total || 0;
-    total = Number(total);
+//    total = Number(total);
     return res.send({ total });
   } catch (error) {
     console.error('Error in GET /total-pledged:', error);
